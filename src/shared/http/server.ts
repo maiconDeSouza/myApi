fdd2b35a152bc31957c9bb30e58f8de2baadc034
Maicon Souza <maicon_souza09@hotmail.com>
2023-01-04 17:07:22 -0300

    add eslint, editorconfig and prettier

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,7 @@
-class AppServer{
-    private app: string
+class AppServer {
+  private app: string
 
-    constructor(info: string){
-        this.app = info ?? `Olá Dev!!!!`
-    }
+  constructor(info: string) {
+    this.app = info ?? `Olá Dev!!!!`
+  }
 }
