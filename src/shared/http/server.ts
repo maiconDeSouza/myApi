9c75b66de72e2c135ca40b662c95877f650b3afa
Maicon Souza <maicon_souza09@hotmail.com>
2023-01-25 20:37:18 -0300

    add libs cors  and express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,19 @@
-console.log('oi')
+import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    message: `Oi`,
+  })
+})
+
+const port = 1992
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
